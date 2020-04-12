64e31776728ad615d9134d775bd6be18305bedfd
LayoutXML <hello@rokasjankunas.com>
2020-04-12 19:50:19 +0300

    Basic functionality of the wheel

--- src/app/modals/projects/projects.component.ts
+++ src/app/modals/projects/projects.component.ts
@@ -1,6 +1,7 @@
 import {Component, HostListener, OnInit} from '@angular/core';
-import {faChevronDown, faChevronUp} from '@fortawesome/free-solid-svg-icons';
+import {faChevronDown, faChevronLeft, faChevronRight, faChevronUp} from '@fortawesome/free-solid-svg-icons';
 import {ProjectsService} from '../../data/projects.service';
+import {Project} from '../../objects/project';
 
 @Component({
   selector: 'app-projects',
@@ -8,32 +9,64 @@ import {ProjectsService} from '../../data/projects.service';
   styleUrls: ['./projects.component.less']
 })
 export class ProjectsComponent implements OnInit {
-  upIcon = faChevronUp;
-  downIcon = faChevronDown;
+  leftIcon = faChevronLeft;
+  rightIcon = faChevronRight;
+
+  data: Project[];
+
   screenWidth = 640;
   isExpanded = false;
+  numberOfCardsShown = 1;
+  minCardIndex = 0;
 
-  constructor(public projectsService: ProjectsService) {
+  constructor(private projectsService: ProjectsService) {
   }
 
   ngOnInit(): void {
     this.screenWidth = window.innerWidth;
+    this.data = this.projectsService.getAllProjects();
+    this.recalculateNumberOfCards();
   }
 
   @HostListener('window:resize', ['$event'])
   onResize(event) {
     this.screenWidth = window.innerWidth;
+    this.recalculateNumberOfCards();
   }
 
-  isProjectDisplayed(index: number) {
-    if (this.isExpanded || index < 3) {
-      return true;
+  getSubsetData(): Project[] {
+    if (this.minCardIndex >= 0 && this.minCardIndex + this.numberOfCardsShown <= this.data.length) {
+      return this.data.slice(this.minCardIndex, this.minCardIndex + this.numberOfCardsShown);
     }
+    let projectList = [];
+    projectList = projectList.concat(this.data.slice(this.minCardIndex, this.data.length));
+    projectList = projectList.concat(this.data.slice(0, this.numberOfCardsShown - projectList.length));
+    return projectList;
+  }
 
-    if (index === 3) {
-      return (this.screenWidth >= 992);
+  recalculateNumberOfCards() {
+    if (this.screenWidth >= 992) {
+      this.numberOfCardsShown = 4;
+    } else if (this.screenWidth >= 768) {
+      this.numberOfCardsShown = 3;
+    } else if (this.screenWidth >= 576) {
+      this.numberOfCardsShown = 2;
+    } else {
+      this.numberOfCardsShown = 1;
     }
+  }
 
-    return false;
+  scrollLeft() {
+    this.minCardIndex--;
+    if (this.minCardIndex <= -this.data.length) {
+      this.minCardIndex = this.data.length - 1;
+    }
+  }
+
+  scrollRight() {
+    this.minCardIndex++;
+    if (this.minCardIndex >= this.data.length) {
+      this.minCardIndex = 0;
+    }
   }
 }
