4659d60bb40dfe620cefdbef39a11f11fc0f3b49
LayoutXML <hello@rokasjankunas.com>
2020-11-16 20:20:55 +0200

    Updates

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,13 +5,13 @@ import {NotFoundComponent} from './pages/not-found/not-found.component';
 import {ProjectViewComponent} from './pages/project/project-view.component';
 
 const routes: Routes = [
-  {path: '', runGuardsAndResolvers: 'always', component: MainComponent},
-  {path: 'home', runGuardsAndResolvers: 'always', redirectTo: '/'},
+  {path: '', runGuardsAndResolvers: 'always', component: MainComponent, },
+  {path: 'home', runGuardsAndResolvers: 'always', redirectTo: '/', pathMatch: 'full'},
   {path: 'projects', runGuardsAndResolvers: 'always', component: MainComponent},
   {path: 'about', runGuardsAndResolvers: 'always', component: MainComponent},
   {path: 'contacts', runGuardsAndResolvers: 'always', component: MainComponent},
   {path: 'projects/:codename', runGuardsAndResolvers: 'always', component: ProjectViewComponent},
-  {path: '**', component: NotFoundComponent, pathMatch: 'full'}
+  {path: '**', component: NotFoundComponent}
 ];
 
 @NgModule({
